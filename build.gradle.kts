66ab081d009e9937ceddf191e8b64940b630e0d7
pmendelski <mendelski.pawel@gmail.com>
2020-08-03 19:15:14 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import pl.allegro.tech.build.axion.release.domain.hooks.HookContext
 import pl.allegro.tech.build.axion.release.domain.hooks.HooksConfig
 import pl.allegro.tech.build.axion.release.domain.scm.ScmPosition
@@ -7,10 +8,10 @@ plugins {
     id("jacoco")
     id("pl.allegro.tech.build.axion-release") version "1.11.0"
     id("com.github.kt3k.coveralls") version "2.10.1"
-    id("com.gradle.plugin-publish") version "0.11.0"
+    id("com.gradle.plugin-publish") version "0.12.0"
     id("java-gradle-plugin")
     id("maven-publish")
-    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
 }
 
 repositories {
@@ -18,8 +19,7 @@ repositories {
 }
 
 ktlint {
-    version.set("0.36.0")
-    enableExperimentalRules.set(true)
+    version.set("0.37.2")
 }
 
 dependencies {
@@ -51,25 +51,27 @@ scmVersion {
 group = "com.coditory.gradle"
 version = scmVersion.version
 
-tasks {
-    withType<Test> {
-        testLogging {
-            events("passed", "failed", "skipped")
-            setExceptionFormat("full")
-        }
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "failed", "skipped")
+        setExceptionFormat("full")
     }
-    withType<Test> {
-        useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.allWarningsAsErrors = true
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
     }
-    jacocoTestReport {
-        reports {
-            xml.isEnabled = true
-            html.isEnabled = true
-        }
-    }
-    coveralls {
-        sourceDirs = listOf("src/main/kotlin")
-    }
+}
+
+coveralls {
+    sourceDirs = listOf("src/main/kotlin")
 }
 
 gradlePlugin {
