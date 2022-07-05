5e8f0585a49ebd106886a58ed25c87f8aea7ab39
Paweł Mendelski <mendelski.pawel@gmail.com>
2022-07-05 15:17:15 +0200

    Fix gradle version parser

--- src/main/kotlin/com/coditory/gradle/integration/SourceSetExtractor.kt
+++ src/main/kotlin/com/coditory/gradle/integration/SourceSetExtractor.kt
@@ -1,10 +1,13 @@
 package com.coditory.gradle.integration
 
+import com.coditory.gradle.integration.shared.SemVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.SourceSetContainer
 
 object SourceSetExtractor {
+    private val GRADLE_VERSION_WITH_EXTENSION = SemVersion.parse("7.1.0")
+
     @Suppress("DEPRECATION")
     fun sourceSets(project: Project): SourceSetContainer {
         return if (usesExtension(project)) {
@@ -17,12 +20,8 @@ object SourceSetExtractor {
     }
 
     private fun usesExtension(project: Project): Boolean {
-        val chunks = project.gradle.gradleVersion.split('.')
-        if (chunks.isEmpty()) {
-            return false
-        }
-        val major = chunks.getOrNull(0)?.toInt() ?: 0
-        val minor = chunks.getOrNull(1)?.toInt() ?: 0
-        return major > 7 || (major == 7 && minor >= 1)
+        val gradleVersion = SemVersion.parseOrNull(project.gradle.gradleVersion)
+            ?: GRADLE_VERSION_WITH_EXTENSION
+        return gradleVersion >= GRADLE_VERSION_WITH_EXTENSION
     }
 }
