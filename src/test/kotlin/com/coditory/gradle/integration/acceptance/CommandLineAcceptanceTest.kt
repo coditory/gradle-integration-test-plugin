7fff9ffea3c55c16d6d3e2a683cd86c236bb8f18
DB <3823705+dineshbhagat@users.noreply.github.com>
2024-08-28 19:29:25 +0530

    Update CommandLineAcceptanceTest.kt

--- src/test/kotlin/com/coditory/gradle/integration/acceptance/CommandLineAcceptanceTest.kt
+++ src/test/kotlin/com/coditory/gradle/integration/acceptance/CommandLineAcceptanceTest.kt
@@ -26,7 +26,7 @@ class CommandLineAcceptanceTest {
                 }
     
                 repositories {
-                    jcenter()
+                    mavenCentral()
                 }
     
                 dependencies {
