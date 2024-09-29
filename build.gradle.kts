b085fc982dce90177021d235f158fb71491e99da
Paweł Mendelski <mendelski.pawel@gmail.com>
2024-09-30 00:46:36 +0200

    Update github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     id("jacoco")
-    id("com.github.kt3k.coveralls") version "2.12.2"
-    id("com.gradle.plugin-publish") version "1.3.0"
     id("java-gradle-plugin")
     id("maven-publish")
+    id("com.gradle.plugin-publish") version "1.3.0"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
@@ -53,8 +52,8 @@ tasks.jacocoTestReport {
     }
 }
 
-coveralls {
-    sourceDirs = listOf("src/main/kotlin")
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 // Marking new version (incrementPatch [default], incrementMinor, incrementMajor)
