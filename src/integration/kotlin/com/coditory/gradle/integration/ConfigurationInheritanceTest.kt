5b31ee00594b0b93786ba31216cb5485d7b0299a
Rafał Harabień <rafalh92@outlook.com>
2024-12-11 10:33:39 +0100

    Make configuration inheritance consistent with Gradle (#165)

---
+++ src/integration/kotlin/com/coditory/gradle/integration/ConfigurationInheritanceTest.kt
@@ -0,0 +1,86 @@
+package com.coditory.gradle.integration
+
+import com.coditory.gradle.integration.base.GradleTestVersions.GRADLE_MAX_SUPPORTED_VERSION
+import com.coditory.gradle.integration.base.GradleTestVersions.GRADLE_MIN_SUPPORTED_VERSION
+import com.coditory.gradle.integration.base.TestProjectBuilder
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.AutoClose
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class ConfigurationInheritanceTest {
+    companion object {
+        @AutoClose
+        private val project = TestProjectBuilder
+            .project("project-${ConfigurationInheritanceTest::class.simpleName}")
+            .withBuildGradleKts(
+                """
+                plugins {
+                    id("com.coditory.integration-test")
+                }
+
+                repositories {
+                    mavenCentral()
+                }
+
+                testing {
+                    suites {
+                        register<JvmTestSuite>("customTest")
+                    }
+                }
+
+                val customTestImplementation by configurations.getting {
+                    extendsFrom(configurations.integrationImplementation.get())
+                }
+                val customTestRuntimeOnly by configurations.getting {
+                    extendsFrom(configurations.integrationRuntimeOnly.get())
+                }
+
+                dependencies {
+                    testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junit}")
+                    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
+                    // sample dependency
+                    implementation("com.google.code.gson:gson:${Versions.gson}")
+                }
+
+                tasks.withType<Test>().configureEach {
+                    useJUnitPlatform()
+                    testLogging {
+                        events("passed", "failed", "skipped")
+                        setExceptionFormat("full")
+                    }
+                }
+                """,
+            ).withFile(
+                "src/customTest/java/TestSpec.java",
+                """
+                import org.junit.jupiter.api.Test;
+                import com.google.gson.Gson;
+
+                public class TestSpec {
+                    @Test
+                    void shouldResolveCompileDependencyFromMainConfig() {
+                        // Gson class import is the actual test
+                    }
+                }
+                """,
+            )
+            .build()
+    }
+
+    @AfterEach
+    fun cleanProject() {
+        project.clean()
+    }
+
+    @ParameterizedTest(name = "should resolve compile dependencies in custom test suite for gradle {0}")
+    @ValueSource(strings = [GRADLE_MAX_SUPPORTED_VERSION, GRADLE_MIN_SUPPORTED_VERSION])
+    fun `should resolve compile dependencies in custom test suite`(gradleVersion: String?) {
+        // when
+        val result = project.runGradle(listOf("customTest"), gradleVersion)
+        // then
+        assertThat(result.task(":customTest")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+    }
+}
