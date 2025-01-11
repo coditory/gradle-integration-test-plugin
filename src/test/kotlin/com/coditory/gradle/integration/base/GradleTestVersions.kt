c000d98215b3d2b4f8564c87f70b9c6d617fd37b
Paweł Mendelski <mendelski.pawel@gmail.com>
2025-01-11 19:24:32 +0100

    Update dependencies and unblock dependabot

--- src/test/kotlin/com/coditory/gradle/integration/base/GradleTestVersions.kt
+++ src/test/kotlin/com/coditory/gradle/integration/base/GradleTestVersions.kt
@@ -2,5 +2,5 @@ package com.coditory.gradle.integration.base
 
 object GradleTestVersions {
     const val GRADLE_MAX_SUPPORTED_VERSION = "current"
-    const val GRADLE_MIN_SUPPORTED_VERSION = "7.6.4"
+    const val GRADLE_MIN_SUPPORTED_VERSION = "8.7"
 }
